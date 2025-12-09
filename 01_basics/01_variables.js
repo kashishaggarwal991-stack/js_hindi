e0fdacc89e6a7a86708c3338f574dadc502a736d
Kashish <kashishaggarwal991@gmail.com>
2025-12-09 22:15:34 +0530

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 248
+let accountEmail="kishii@gmail.com"
+var accountPassword = "kri"
+accountCity = "Allahbad"
+
+accountPassword  = "hello"
+// accountID = 2
+accountEmail= "priya@123"
+accountCity = "Gujarat"
+console.log(accountID);
+
+console.table([accountEmail,accountPassword,accountCity])
+
