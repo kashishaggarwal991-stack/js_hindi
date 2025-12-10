842c969c788df8e1f50171bf41d043c0d704ce43
Kashish <kashishaggarwal991@gmail.com>
2025-12-10 15:59:14 +0530

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let name = "Kashish"
 let object = Number(name)
-console.log(typeof object)
-console.log(object)
+// console.log(typeof object)
+// console.log(object)
 
 // 33 -> 33
 // "33abc"  -> NaN
@@ -10,7 +10,7 @@ console.log(object)
 
 let isChecked = true
 let booleanisChecked = Boolean(isChecked)
-console.log(booleanisChecked)
+// console.log(booleanisChecked)
 
 // true -> 1 , false ->0
 // "any name" -> true
@@ -18,7 +18,30 @@ console.log(booleanisChecked)
 
 let someNumber = 35
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
 
 // 35 ->35 (type is string)
+
+// ****************** Operations ***************
+
+let num = 33
+let negativenum = -num
+// console.log(negativenum);
+
+let str1 = "Hello"
+let str2 = " Ram"
+let str3 = str1+str2
+console.log(str3);
+
+// console.log("1"+2);  12
+// console.log(1+"2");  12
+// console.log("1"+2+3); 123
+// console.log(3+2+"1");  51
+// console.log(+true);
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+//console.log("2">1)
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null >=0)
+
+console.log(undefined>0)
+console.log(undefined == 0)
+console.log(undefined >=0)
+
+
+// === strictly checks the datatypes 
+console.log("2"===2)
