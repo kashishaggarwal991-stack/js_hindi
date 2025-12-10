2990c579ad748123958d104d3cfd35d970e56948
Kashish <kashishaggarwal991@gmail.com>
2025-12-11 00:05:54 +0530

    maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,33 @@
+const balance = 100
+console.log(balance);
+const cash = new Number(120)
+// 
+const one = 123.868
+// console.log(one.toPrecision(3));
+
+const two = 10000000
+// console.log(two.toLocaleString());
+
+//+++++++++++  MATHS  //////////////////
+
+/* console.log(Math);
+console.log(Math.abs(-5)); // it will change negative value in positive one
+console.log(Math.round(4.4));
+console.log(Math.floor(5.1));
+console.log(Math.ceil(5.1));
+console.log(Math.min(1,2,3,4)); */
+
+console.log(Math.random()); // it will give random values from 0 to 1
+console.log(Math.random() * 10 );
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1)+min));
+
+
+
+
+
+
+
+
