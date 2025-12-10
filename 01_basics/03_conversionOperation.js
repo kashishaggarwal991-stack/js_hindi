f90249f8bb5f9b1aae11e47c4a8aab54894ec66b
Kashish <kashishaggarwal991@gmail.com>
2025-12-10 15:06:08 +0530

    Datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let name = "Kashish"
+let object = Number(name)
+console.log(typeof object)
+console.log(object)
+
+// 33 -> 33
+// "33abc"  -> NaN
+// true ->1
+// false ->0
+
+let isChecked = true
+let booleanisChecked = Boolean(isChecked)
+console.log(booleanisChecked)
+
+// true -> 1 , false ->0
+// "any name" -> true
+// ""  -> false
+
+let someNumber = 35
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+// 35 ->35 (type is string)
