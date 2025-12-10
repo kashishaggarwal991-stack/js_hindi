c188e4f6c1986fa8bad78b45551d16dbb73bd548
Kashish <kashishaggarwal991@gmail.com>
2025-12-10 23:24:38 +0530

    strings

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 types : String,Number,Boolean,null,undefined,symbol,BigInt
+
+// Reference(Non-Primitive)
+// Objects , Array , Functions
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+/* console.log(Id==anotherId);
+console.log(typeof anotherId)  */
+
+// Stack (Primitive)  and Heap(non-Primitive)
+let myName = "Kashish"
+let anotherName = myName
+anotherName = "Riya"
+console.log(anotherName); // here copy is created
+
+// heap
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo =  userOne  // here reference is passed
+userTwo.email = "kishii@gmail.com" 
+console.log(userOne.email);
+console.log(userTwo.email);
+console.log(userTwo)
+
+
+
+
---
+++ 01_basics/strings.js
@@ -0,0 +1,32 @@
+const name = "Kashish"
+const repoCount = 10
+// console.log(name+repoCount+ " hello");
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const game = new String("hite-shhc")
+/*   console.log(game[0]);
+console.log(game.__proto__);
+console.log(game.length);
+console.log(game.charAt(4));
+console.log(game.indexOf('i'));
+console.log(game.toUpperCase());   */
+/* console.log(game.charAt(4));
+
+const newString = game.substring(0,4)
+console.log(newString);  // only characters frpm 0 to 3 will come
+const nextString = game.slice(-8,4) // negative values are also accepted
+console.log(nextString )  */
+
+const one = "  hitesh  "
+console.log(one.trim()) // it removes whitespace character
+
+const two = "kashish@gmail.com/kashish%20gmail"
+console.log(two.replace('%20','-'));  // replace a character
+
+console.log(game.includes("hitr")); // checks if element is present or  not in string
+
+console.log(game.split("-")); // splits string on basis of character we give
+
+
+
+
